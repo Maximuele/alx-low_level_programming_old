2b54b5687b1c99ef6b010512413dbdd79cd5cad3
Maximuele <markokots@gmail.com>
2022-06-30 13:51:52 -0700

    c program that prints with Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
-/ * main entry point
+ /* main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (sucess)
- *
  */
-
  int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
-
-
