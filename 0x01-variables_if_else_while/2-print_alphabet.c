6b3bb076baf13121bd5b89b2c17f4e928bacda88
Maximuele <markokots@gmail.com>
2022-07-01 15:33:32 -0700

    print alplabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alpahbet in lower case
+ * then new line
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
