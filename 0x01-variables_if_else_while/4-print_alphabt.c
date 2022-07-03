c023f0deee7662941615d7e491624a02349a42b1
Maximuele <markokots@gmail.com>
2022-07-03 14:28:18 -0700

    printing lower case alphabet with exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,6 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	if (letter != 'e' && letter != 'q')
-		putchar(letter);
-	putchar('\n');
+		putchar(letter, '\n');
 	return (0);
 }
