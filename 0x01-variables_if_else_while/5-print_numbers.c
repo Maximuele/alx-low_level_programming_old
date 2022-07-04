062dee5432b0a4dac2fa7f66e825771d567b7c2c
Maximuele <markokots@gmail.com>
2022-07-04 05:50:36 -0700

    C code to print single digit decimal

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing all single digits number of base 10
+ * starting with 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; n < 10; ++num)
+		printf('%d', num);
+		printf("\n");
+	return (0);
